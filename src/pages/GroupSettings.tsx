03fe8c0a1404bee7d92a938b996555894e39fb28
Mary Kate Fain <mk@marykatefain.com>
2025-05-23 11:09:09 +0200

    make members notif blue

--- src/pages/GroupSettings.tsx
+++ src/pages/GroupSettings.tsx
@@ -438,8 +438,7 @@ export default function GroupSettings() {
                 Members
                 {pendingRequestsCount > 0 && (
                   <Badge 
-                    variant="destructive" 
-                    className="absolute -top-2 -right-2 h-5 w-5 p-0 flex items-center justify-center text-xs z-10"
+                    className="absolute -top-2 -right-2 h-5 w-5 p-0 flex items-center justify-center text-xs bg-blue-500 hover:bg-blue-600 z-10"
                   >
                     {pendingRequestsCount > 99 ? '99+' : pendingRequestsCount}
                   </Badge>
